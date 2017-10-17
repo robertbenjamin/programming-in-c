8e03ceff9cb8148ceded348e3f7b4507db776b6b
Robert Benjamin <rotberj@gmail.com>
2017-10-16 17:53:34 -0700

    Initial commit

---
+++ chapter2/exc4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int value1, value2, result;
+
+  value1 = 87;
+  value2 = 15;
+
+  result = value1 - value2;
+
+  printf("%i minus %i equals %i.\n", value1, value2, result);
+
+  return 0;
+}
---
+++ chapter2/exc5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int sum;
+
+  // Compute result
+  sum = 25 + 37 - 19;
+
+  // Display results
+  printf("The answer is %i.\n", sum);
+
+  return 0;
+}
