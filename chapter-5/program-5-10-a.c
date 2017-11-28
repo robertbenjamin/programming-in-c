3c9de6ebe495b2ca662f66469585199239fcc7c0
Robert Benjamin <rotberj@gmail.com>
2017-11-28 10:52:37 -0800

    Update program 5.10, add program 5.10.a

--- chapter-5/program-5-10-a.c
+++ chapter-5/program-5-10-a.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void) {
-  _Bool isPrime;
+  bool isPrime;
 
   for (int p = 2; p <= 50; p++) {
-    isPrime = 1;
+    isPrime = true;
 
     for (int d = 2; d < p; d++) {
       if (p % d == 0) {
-        isPrime = 0;
+        isPrime = false;
         break;
       }
     }
 
-    if (isPrime != 0) {
+    if (isPrime) {
       printf("%i  ", p);
     }
   }
